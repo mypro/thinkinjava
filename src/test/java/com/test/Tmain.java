6f975301f3a9e34c8689fe640f89af5ce0d58c68
chenweichao <chenweichao@news.cn>
2014-09-25 09:44:31 +0800

    测试类

---
+++ src/test/java/com/test/Tmain.java
@@ -0,0 +1,26 @@
+package com.test;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import thinkInjava.c4.Flower;
+
+public class Tmain {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("before test####################################################################");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("After  test####################################################################");
+	}
+
+	@Test
+	public void testFlower() {
+
+		Flower x = new Flower();
+		x.print();
+	}
+}
