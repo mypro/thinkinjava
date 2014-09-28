6290335ff110c3ddccfae73521363a49d08a0e05
chenweichao <chenweichao@news.cn>
2014-09-28 17:32:13 +0800

    Music

--- src/test/java/thinkInjava/c7/Music.java
+++ src/test/java/thinkInjava/c7/Music.java
@@ -23,13 +23,44 @@ class Wind extends Instrument {
 	}
 }
 
+class Stringed extends Instrument {
+	@Override
+	public void play(Note n) {
+		System.out.println("Stringed.play()");
+	}
+}
+
+class Brass extends Instrument {
+	@Override
+	public void play(Note n) {
+		System.out.println("Brass.play()");
+	}
+}
+
 public class Music {
-	public static void tune(Instrument i) {
+	// public static void tune(Instrument i) {
+	// i.play(Note.middleC);
+	// }
+
+	public static void tune(Wind i) {
+		i.play(Note.middleC);
+	}
+
+	public static void tune(Stringed i) {
+		i.play(Note.middleC);
+	}
+
+	public static void tune(Brass i) {
 		i.play(Note.middleC);
 	}
 
 	public static void main(String[] args) {
 		Wind flute = new Wind();
-		tune(flute);
+		Stringed violin = new Stringed();
+		Brass frenchHorn = new Brass();
+		tune(flute); // No upcasting
+		tune(violin);
+		tune(frenchHorn);
+
 	}
 }
