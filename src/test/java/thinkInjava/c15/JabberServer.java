ab5d0be1ed8edb5063fb5a3660e61dc77447162c
chenweichao <chenweichao@news.cn>
2014-10-27 17:20:36 +0800

    c15  server  client

---
+++ src/test/java/thinkInjava/c15/JabberServer.java
@@ -0,0 +1,88 @@
+package thinkInjava.c15;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class JabberServer {
+	public static final int PORT = 8080;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("before test##########################################");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("AfterClass test##########################################");
+	}
+
+	@Test
+	public void test() throws Exception {
+		ServerSocket s = new ServerSocket(PORT);
+		System.out.println("Started: " + s);
+		try {
+			Socket socket = s.accept();
+			try {
+				System.out.println("Connection accepted : " + socket);
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),
+						true);
+
+				while (true) {
+					String str = in.readLine();
+					if (str.equals("END")) {
+						break;
+					}
+					System.out.println("Echoing: " + str);
+					out.println(str);
+				}
+			} finally {
+				System.out.println("closing...");
+				socket.close();
+			}
+		} finally {
+			s.close();
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		ServerSocket s = new ServerSocket(PORT);
+		System.out.println("Started: " + s);
+		try {
+			Socket socket = s.accept();
+			try {
+				System.out.println("Connection accepted : " + socket);
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),
+						true);
+
+				while (true) {
+					String str = in.readLine();
+					if (str.equals("END")) {
+						break;
+					}
+					System.out.println("Echoing: " + str);
+					out.println(str);
+				}
+			} finally {
+				System.out.println("closing...");
+				socket.close();
+			}
+		} finally {
+			s.close();
+		}
+
+	}
+}
